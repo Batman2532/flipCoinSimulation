3fdc213516096fab64e833a4de3dca921143bc77
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-07 16:43:37 +0530

    Added: Multiple time of fliping

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-flipCoin=$((RANDOM%2))
-if [ $flipCoin -eq 1 ]
-then
-	echo Heads
-else
-	echo Tails
-fi
+heads=0
+tails=0
+for((i=1;i<21;i++))
+do
+
+	flipCoin=$((RANDOM%2))
+	if [ $flipCoin -eq 1 ]
+	then
+		(( heads++ ))
+	else
+		(( tails++ ))
+	fi
+done
+echo "Heads won: "$heads "Tails won: " $tails
